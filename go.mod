eeb116113614c71f5034501dee12b9a92f7d36ef
Derek Wright <dwright@eitccorp.com>
2025-08-09 15:30:11 -0400

    Added generic elements

--- go.mod
+++ go.mod
@@ -2,8 +2,6 @@ module github.com/derekmwright/htemel
 
 go 1.24.5
 
-require (
-	golang.org/x/net v0.43.0
-)
+require golang.org/x/net v0.43.0
 
-require golang.org/x/text v0.28.0 // indirect
+require golang.org/x/text v0.28.0
--- node.go
+++ node.go
@@ -1,7 +1,70 @@
 package htemel
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Node interface {
 	Render(w io.Writer) error
+}
+
+type GroupElement struct {
+	children []Node
+}
+
+func Group(children ...Node) *GroupElement {
+	return &GroupElement{children}
+}
+
+func (e *GroupElement) Render(w io.Writer) error {
+	for _, child := range e.children {
+		if err := child.Render(w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type GenericElement struct {
+	tag      string
+	attrs    map[string]any
+	children []Node
+}
+
+func Generic(tag string, attrs map[string]any, children ...Node) *GenericElement {
+	return &GenericElement{
+		tag:      tag,
+		attrs:    attrs,
+		children: children,
+	}
+}
+
+func (e *GenericElement) Render(w io.Writer) error {
+	if _, err := w.Write([]byte("<" + e.tag)); err != nil {
+		return err
+	}
+
+	for key, val := range e.attrs {
+		if _, err := w.Write([]byte(fmt.Sprintf(" %s=\"%v\"", key, val))); err != nil {
+			return err
+		}
+	}
+
+	if _, err := w.Write([]byte(">")); err != nil {
+		return err
+	}
+
+	for _, child := range e.children {
+		if err := child.Render(w); err != nil {
+			return err
+		}
+	}
+
+	if _, err := w.Write([]byte("</" + e.tag + ">")); err != nil {
+		return err
+	}
+
+	return nil
 }
